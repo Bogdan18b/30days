955796c4f20c0e128a0e54613c9df9841cece4ae
bogdan <bogdanbobletec@gmail.com>
2019-01-18 08:24:22 -0500

    Day 14 end

--- ReferencesVsCopying/index.js
+++ ReferencesVsCopying/index.js
@@ -1,7 +1,20 @@
     // start with strings, numbers and booleans
+    let x = 5;
+    let y = x;
+    console.log(x, y);
+    x = 2;
+    console.log(x, y);
+
+    let name = 'bob';
+    let name2 = name;
+    console.log(name, name2);
+    name = 'ion';
+    console.log(name, name2);
 
     // Let's say we have an array
     const players = ['Bogdan', 'Cristi', 'Mariella', 'Claudiu'];
+    const team = players;
+    console.log(players, team);
 
     // and we want to make a copy of it.
 
@@ -16,14 +29,17 @@
     // Why? It's because that is an array reference, not an array copy. They both point to the same array!
 
     // So, how do we fix this? We take a copy instead!
-
     // one way
+    let team2 = players.slice();
 
     // or create a new array and concat the old one in
-
+    let team3 = [].concat(team);
     // or use the new ES6 Spread
-
+    let team4 = [...team];
     // now when we update it, the original one isn't changed
+    let team5 = Array.from(players);
+    team[3] = 'Ion';
+    console.log(players, team);
 
     // The same thing goes for objects, let's say we have a person object
 
@@ -34,9 +50,15 @@
     };
 
     // and think we make a copy:
-
+    const captain = person;
+    captain.number = 18;
+    console.log(person, captain);
     // how do we take a copy instead?
-
-    // We will hopefully soon see the object ...spread
-
+    const cap2 = Object.assign({}, person, { age: 30 });
+    console.log(cap2);
+    // We also have the object spread
+    const cap3 = {...person, age: 23};
+    console.log(cap3);
     // Things to note - this is only 1 level deep - both for Arrays and Objects. lodash has a cloneDeep method, but you should think twice before using it.
+
+    const PoorMansDeepClone = JSON.parse(JSON.stringify(person));
